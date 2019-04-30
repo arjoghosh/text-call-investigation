96a80295fb209d10a10a9b63a078df04614a22a1
Phien Pham <phiendpham@gmail.com>
2019-04-30 12:45:45 +0700

    Implement Task 0

--- Task0.py
+++ Task0.py
@@ -20,3 +20,15 @@ Print messages:
 "Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
 """
 
+# Print the first record of texts
+text_incoming_number = texts[0][0]
+text_answering_number = texts[0][1]
+text_time = texts[0][2]
+print('First record of texts, {} texts {} at time {}'.format(text_incoming_number, text_answering_number, text_time))
+
+# Print the last record of calls
+call_incoming_number = calls[-1][0]
+call_answering_number = calls[-1][1]
+call_time = calls[-1][2]
+call_duration = calls[-1][3]
+print('Last record of calls, {} calls {} at time {}, lasting {} seconds'.format(call_incoming_number, call_answering_number, call_time, call_duration))
