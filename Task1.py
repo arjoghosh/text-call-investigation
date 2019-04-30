993ed24c5afedcca98d74253a978db9c3f0081db
Phien Pham <phiendpham@gmail.com>
2019-04-30 14:22:22 +0700

    Implement Task 1

--- Task1.py
+++ Task1.py
@@ -14,7 +14,21 @@ with open('calls.csv', 'r') as f:
 
 """
 TASK 1:
-How many different telephone numbers are there in the records? 
+How many different telephone numbers are there in the records?
 Print a message:
 "There are <count> different telephone numbers in the records."
 """
+
+# For each incoming and answering number in texts and calls database,
+# we add the numbers into a set, so that the unique phone number will be saved.
+phone_numbers = set()
+
+for record in texts:
+    phone_numbers.add(record[0])
+    phone_numbers.add(record[1])
+
+for record in calls:
+    phone_numbers.add(record[0])
+    phone_numbers.add(record[1])
+
+print("There are {} different telephone numbers in the record".format(len(phone_numbers)))
